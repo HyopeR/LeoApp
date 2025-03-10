df7aa57f1666d43251c78e26abd4a2944eae839a
Tolgahan Çelik <tol_celik@hotmail.com>
2025-03-10 03:47:31 +0300

    Eslint, prettier configuration, environment arragements, react connected electron, scripts arragements, project start configurations.

---
+++ src/app/main.ts
@@ -0,0 +1,13 @@
+import { app, BrowserWindow } from 'electron';
+import { isDev } from './utils.js';
+import * as path from 'node:path';
+
+app.on('ready', () => {
+  const window = new BrowserWindow({});
+
+  if (isDev()) {
+    window.loadURL('http://localhost:5001');
+  } else {
+    window.loadFile(path.join(app.getAppPath(), '/dist-web/index.html'));
+  }
+});
